5f5c139d2d09d70e4598193bfd465a3a6ea96b62
imadhrit <maharajrabel@gmail.com>
2021-10-26 05:17:42 +0530

    test

---
+++ test/domainfinder_test.go
@@ -0,0 +1,26 @@
+package test 
+
+import (
+	"context"
+	"testing"
+	"github.com/Zumpit/domainfinder"
+)
+
+var ctx = context.Background()
+
+func TestSearch(t *testing.T){
+	q := "\"allied infoline\""
+
+	opts := domainfinder.SearchOptions{
+		Limit: 20,
+	}
+	ret, err := domainfinder.Search(ctx, q,opts)
+	if err != nil {
+			t.Errorf("something went wrong : %v", err)
+			return 
+	}
+
+	if len(ret) == 0 {
+		t.Errorf("no results found : %v", ret)
+	}	
+}
